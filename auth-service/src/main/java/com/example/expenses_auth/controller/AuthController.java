532ae12722cc67223aede5a6a5b6aa82d98c7331
patlolavigneshwarreddy <patlolavigneshwarreddy@gmail.com>
2026-03-04 12:54:49 +0530

    added new logs dependency and sone data in propertie file

--- auth-service/src/main/java/com/example/expenses_auth/controller/AuthController.java
+++ auth-service/src/main/java/com/example/expenses_auth/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.expenses_auth.controller;
 
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.expenses_auth.security.CustomUserDetails;
 import com.example.expenses_auth.security.JwtUtil;
 
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.*;
 
 @RestController
 @RequestMapping("/auth")
 @Slf4j
 public class AuthController {
 
+	private static final org.slf4j.Logger logger =
+            LoggerFactory.getLogger(AuthController.class);
+	
 	@Autowired
 	private AuthenticationManager authManager;
 
@@ -33,7 +39,9 @@ public class AuthController {
 	@PostMapping("/login")
 	public ResponseEntity<String> login(@Validated @RequestBody LoginRequest req) {
 //		log.info("Login API: authentication");
-
+		logger.info("User try to login with : "+req);
+        logger.warn("User try to login with : "+req);
+        logger.error("User try to login with : "+req);
 		try {
 			Authentication auth = authManager
 					.authenticate(new UsernamePasswordAuthenticationToken(req.username(), req.password()));
